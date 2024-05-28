da9dd2d5579b26cc355e306758ddb39e7c343794
Chris Lundberg <chris@frakture.com>
2024-05-28 08:38:50 -0400

    Override dependencies to get rid of audit flag

--- test/packet/zip.js
+++ test/packet/zip.js
@@ -4,15 +4,15 @@ const assert = require('node:assert');
 const { create, getManifest } = require('../../index');
 
 describe('Test Person Packet Creator', async () => {
-  it('should create a zip file', async () => {
+  const pfile = './test/sample/message/5_fake_people.csv';
+  it(`should create a zip file from directory ${process.cwd()} with path ${pfile}`, async () => {
     await create({
-      personFiles: ['test/sample/message/5_fake_people.csv'],
+      personFiles: [pfile],
       messageFiles: 'test/sample/message/message.json5',
     });
   });
   it('should retrieve a manifest', async () => {
-    const manifest = await getManifest({ packet: 'test/sample/message.packet.zip' });
-    // console.log('Manifest=', typeof manifest, manifest);
+    const manifest = await getManifest({ packet: './test/sample/5_message.packet.zip' });
     assert.equal(typeof manifest, 'object', 'Manifest is not an object');
     assert.equal(manifest.accountId, 'engine9', 'Manifest does not have an accountId=engine9');
   });
